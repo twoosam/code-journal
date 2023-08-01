c521229ae9bcc2f7db06838d82ed27b2b78ba7a4
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-01 18:41:08 +0000

    Added a submit event lsitener to the form.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@ function updateValue() {
   document.getElementById('img').setAttribute('src', $photoInput.value);
 }
 $photoInput.addEventListener('input', updateValue);
+
+const $submit = document.querySelector('#form');
+function submitForm(event) {
+  event.preventDefault();
+  const title = $submit.elements.title.value;
+  const photoURL = $submit.elements.photoURL.value;
+  const notes = $submit.elements.notes.value;
+  const formObject = {
+    title,
+    photoURL,
+    notes,
+  };
+  formObject.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(formObject);
+  document
+    .getElementById('img')
+    .setAttribute('src', 'images/placeholder-image-square.jpg');
+  $submit.reset();
+}
+$submit.addEventListener('submit', submitForm);

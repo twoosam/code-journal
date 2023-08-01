a052fa4e861f800cbf0c51b43a08e97d47d00dc6
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-01 23:45:57 +0000

    Removed unnecessary new variable.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ function updateValue() {
 $photoInput.addEventListener('input', updateValue);
 
 const $submit = document.querySelector('#form');
-const $imgPlaceholder = document.getElementById('img');
 function submitForm(event) {
   event.preventDefault();
   const title = $submit.elements.title.value;
@@ -20,7 +19,7 @@ function submitForm(event) {
   formObject.entryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(formObject);
-  $imgPlaceholder.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $submit.reset();
 }
 $submit.addEventListener('submit', submitForm);

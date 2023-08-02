6acc3d1f9ebaa42746932a54291e1f19896ce845
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-02 16:40:28 +0000

    Defined a function named renderEntry with parameter entry that generates and returns a DOM tree for a single entry.

--- js/main.js
+++ js/main.js
@@ -23,3 +23,48 @@ function submitForm(event) {
   $submit.reset();
 }
 $submit.addEventListener('submit', submitForm);
+
+function renderEntry(entry) {
+  const $divDataView = document.createElement('div');
+  $divDataView.setAttribute('data-view', 'entries');
+
+  const $ul = document.createElement('ul');
+  $divDataView.appendChild($ul);
+
+  const $li = document.createElement('li');
+  $ul.appendChild($li);
+
+  const $divRow = document.createElement('div');
+  $divRow.setAttribute('class', 'row');
+  $li.appendChild($divRow);
+
+  const $divCol = document.createElement('div');
+  $divCol.setAttribute('class', 'column-full');
+  $divCol.setAttribute('class', 'column-half');
+  $divRow.appendChild($divCol);
+
+  const $imgView = document.createElement('img');
+  $imgView.setAttribute('class', 'img-view-entry');
+  $imgView.setAttribute(
+    'src',
+    'https://images.immediate.co.uk/production/volatile/sites/3/2017/12/yoda-the-empire-strikes-back-28a7558.jpg?quality=90&resize=800,534'
+  );
+  $imgView.setAttribute('alt', 'yoda');
+  $divCol.appendChild($imgView);
+
+  const $divColLower = document.createElement('div');
+  $divColLower.setAttribute('class', 'column-full');
+  $divColLower.setAttribute('class', 'column-half');
+  $divRow.appendChild($divColLower);
+
+  const $h2 = document.createElement('h2');
+  $h2.textContent = 'A newer image';
+  $divColLower.appendChild($h2);
+
+  const $p = document.createElement('p');
+  $p.textContent = 'An old and powerful jedi.';
+  $divColLower.appendChild($p);
+
+  return $divDataView;
+}
+renderEntry();
